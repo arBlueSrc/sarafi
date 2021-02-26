a20a44a0b4958bbdecef05cc1eeb9fda9dd4fe1d
arash <arashmirzaie.1997@gmail.com>
2021-02-26 12:39:02 +0330

    info fragment

--- app/src/main/java/com/arash/applikatask/ui/home/HomeFragment.kt
+++ app/src/main/java/com/arash/applikatask/ui/home/HomeFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.arash.applikatask.R
 import com.arash.applikatask.databinding.HomeFragmentBinding
 import com.arash.applikatask.localdb.PriceRepository
 import com.arash.applikatask.model.localdbmodel.DbModel
@@ -31,14 +33,23 @@ class HomeFragment : BaseFragment() {
         super.onViewCreated(view, savedInstanceState)
 
 
-        PriceRepository.getFilterPrice(requireContext(), "arz")?.observe(viewLifecycleOwner, {
-            binding.textView5.text =
-                it.filter { filter -> filter.Name.equals("دلار") }?.get(0)?.Price.let {
-                    "قیمت دلار امروز : $it تومان"
-                }
-        })
+        //update dollar price in home screen
+        updateDollarPrice()
+
+        //observe retrofit getting data and put it into room
+        observeData()
 
 
+        //click update btn will get data from server and put it into room
+        onUpdateClick()
+
+
+        //on info button clicked
+        onInfoClick()
+
+    }
+
+    private fun observeData() {
         viewModel.responseArz.observe(viewLifecycleOwner, {
             binding.textView5.text =
                 viewModel.responseArz.value?.filter { it.name.equals("دلار") }?.get(0)?.price.let {
@@ -58,6 +69,8 @@ class HomeFragment : BaseFragment() {
                 viewModel.insertData(requireContext(), saveItem)
             }
         })
+
+
 
         viewModel.responseSeke.observe(viewLifecycleOwner, {
 
@@ -90,11 +103,28 @@ class HomeFragment : BaseFragment() {
                 viewModel.insertData(requireContext(), saveItem)
             }
         })
+    }
 
+    private fun updateDollarPrice() {
+
+        PriceRepository.getFilterPrice(requireContext(), "arz")?.observe(viewLifecycleOwner, {
+            binding.textView5.text =
+                it.filter { filter -> filter.Name.equals("دلار") }.get(0).Price.let {
+                    "قیمت دلار امروز : $it تومان"
+                }
+        })
+    }
+
+    private fun onUpdateClick() {
         binding.button.setOnClickListener {
             viewModel.getData()
         }
+    }
 
+    private fun onInfoClick() {
+        binding.btnInfo.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_infoFragment)
+        }
     }
 
 }
--- app/src/main/java/com/arash/applikatask/ui/info/InfoFragment.kt
+++ app/src/main/java/com/arash/applikatask/ui/info/InfoFragment.kt
@@ -1,9 +1,14 @@
 package com.arash.applikatask.ui.info
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.navigation.fragment.findNavController
 import com.arash.applikatask.databinding.FragmentInfoBinding
 import com.arash.applikatask.utils.bases.BaseFragment
 
@@ -22,6 +27,77 @@ class InfoFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        onBackPress()
+        onBackShape()
+
+        onInstagramClick()
+
+        onLinkedinClick()
+
+    }
+
+    private fun onBackShape() {
+        binding.btnBack.setOnClickListener {
+            findNavController().navigateUp()
+        }
+    }
+
+    private fun onLinkedinClick() {
+        binding.btnLinkedin.setOnClickListener {
+            onLinkedin()
+        }
+    }
+
+    private fun onLinkedin() {
+        val uri: Uri = Uri.parse("https://ir.linkedin.com/public-profile/in/arash-mirzaie")
+        val likeIng = Intent(Intent.ACTION_VIEW, uri)
+
+        likeIng.setPackage("com.linkedin.android")
+
+        try {
+            startActivity(likeIng)
+        } catch (e: ActivityNotFoundException) {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://ir.linkedin.com/public-profile/in/arash-mirzaie?challengeId=AQHMSx6vPyyGpAAAAXe6L5U1G9DEsp4P34-msgWmf1sd3XWvTs1WvTLVgrXROQusEOGQysQzXg2kqGTaK885-cI_dQgY_iiurg&submissionId=7589137d-b824-6516-ba18-92737e280467")
+                )
+            )
+        }
+    }
+
+    private fun onInstagramClick() {
+        binding.btnInsta.setOnClickListener {
+            onInstagram()
+        }
+    }
+
+    private fun onInstagram() {
+        val uri: Uri = Uri.parse("https://www.instagram.com/arash.mirzaie.en")
+        val likeIng = Intent(Intent.ACTION_VIEW, uri)
+
+        likeIng.setPackage("com.instagram.android")
+
+        try {
+            startActivity(likeIng)
+        } catch (e: ActivityNotFoundException) {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("http://instagram.com/arash.mirzaie.en")
+                )
+            )
+        }
+    }
+
+    fun onBackPress(){
+        val callback: OnBackPressedCallback =
+            object : OnBackPressedCallback(true /* enabled by default */) {
+                override fun handleOnBackPressed() {
+                    findNavController().navigateUp()
+                }
+            }
+        requireActivity().onBackPressedDispatcher.addCallback(requireActivity(), callback)
     }
 
 }
